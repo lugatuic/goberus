b890cffa67d0867f650c666f37e4c047d17d19fb
operatr <metatron64@outlook.com>
2025-11-23 05:54:31 -0600

    Escape additional DN chars

--- ldaps/dn.go
+++ ldaps/dn.go
@@ -10,7 +10,7 @@ func escapeDNComponent(s string) string {
 	for i, r := range s {
 		isSpecial := false
 		switch r {
-		case '\\', ',', '+', '"', '<', '>', ';':
+		case '\\', ',', '+', '"', '<', '>', ';', '#', '=':
 			isSpecial = true
 		}
 
